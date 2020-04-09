9186c36f49f53509e0492ac71fb44a9a3bec0dbc
asis-parajuli <60426026+asis-parajuli@users.noreply.github.com>
2020-04-09 22:50:44 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Book',
+        image: './assets/python-book.png',
+        inStock: true,
+        details: ["Fabrizio Romano", "December 2015" ]
+    }
+})
